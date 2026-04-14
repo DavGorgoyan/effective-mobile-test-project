5b0ff0edcec399b66ef0d3e2b41bb7c4e55f9a78
Dav <gorgoyandav@gmail.com>
2026-04-14 17:25:22 +0400

    style: :art: Routes: default to "/"

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ const createApp = (): Express => {
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: true }));
 
-  app.use("/api", routes);
+  app.use("/", routes);
 
   app.use(notFound);
   app.use(errorHandler);
